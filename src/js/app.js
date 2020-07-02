2c1290cd8c539f6f17174ba3dfd89a68c3e99b70
Joe Grace <joeggrace@gmail.com>
2020-07-01 21:00:54 -0400

    auto-clicker and multiplier working

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,33 @@
 const donut = new Donut();
-const donutButton = document.querySelector('.donut-button');
-const clickerButton = document.querySelector('.clicker-button');
+const donutButton = document.querySelector(".donut-button");
+const clickerButton = document.querySelector(".clicker-button");
+const multiplierButton = document.querySelector(".multiplier-button");
 
-donutButton.addEventListener('click', () => {
-    event.preventDefault();
+donutButton.addEventListener("click", () => {
+  event.preventDefault();
+  donut.donutClick();
+  donut.updateDonutCount();
+});
+
+clickerButton.addEventListener("click", () => {
+  event.preventDefault();
+  donut.buyAutoClicker();
+  donut.updateAutoClicker();
+  donut.updateDonutCount();
+});
+
+multiplierButton.addEventListener("click", () => {
+  event.preventDefault();
+  donut.buyMultiplier();
+  donut.updateDonutMultiplier();
+  donut.updateDonutCount();
+});
+
+function autoClick() {
+  for (let i = 0; i < donut.autoClickers; i++) {
     donut.donutClick();
-    donut.updateDonutCount();
-});
+  }
+  donut.updateDonutCount();
+}
 
-clickerButton.addEventListener('click', () => {
-    event.preventDefault();
-    donut.buyAutoClicker();
-    donut.updateAutoClicker();
-    donut.updateDonutCount();
-});
+setInterval(autoClick, 1000);
--- src/js/donut.js
+++ src/js/donut.js
@@ -3,34 +3,64 @@ class Donut {
     this.count = 0;
     this.autoClickers = 0;
     this.autoClickersCost = 100;
+    this.multiplier = 1;
+    this.multiplierAmount = 0;
+    this.multiplierCost = 10;
   }
   donutClick() {
-    this.count += 1;
+    this.count += Math.ceil(this.multiplier);
   }
   buyAutoClicker() {
-    this.count = Math.round((this.count)-(this.autoClickersCost));
+    this.count = Math.round(this.count - this.autoClickersCost);
     this.autoClickers += 1;
   }
-  showAutoClickerButton(){
+  buyMultiplier() {
+    this.count = Math.round(this.count - this.multiplierCost);
+    this.multiplier = this.multiplier * 1.2;
+    this.multiplierAmount += 1;
+  }
+  showAutoClickerButton() {
     clickerButton.classList.add("clicker-button-on");
   }
-  hideAutoClickerButton(){
+  hideAutoClickerButton() {
     clickerButton.classList.remove("clicker-button-on");
   }
-  updateAutoClicker(){
-      const numberOfClicker = document.querySelector(".amount-of-clickers");
-      const clickerCost = document.querySelector(".clicker-cost");
-      numberOfClicker.innerText = this.autoClickers;
-      this.autoClickersCost = (this.autoClickersCost * 1.1);
-      clickerCost.innerText = Math.round(this.autoClickersCost);
+  showMultiplierButton() {
+    multiplierButton.classList.add("multiplier-button-on");
+  }
+  hideMultiplierButton() {
+    multiplierButton.classList.remove("multiplier-button-on");
+  }
+  updateAutoClicker() {
+    const numberOfClicker = document.querySelector(".amount-of-clickers");
+    const clickerCost = document.querySelector(".clicker-cost");
+    numberOfClicker.innerText = this.autoClickers;
+    this.autoClickersCost = this.autoClickersCost * 1.1;
+    clickerCost.innerText = Math.round(this.autoClickersCost);
+  }
+  updateDonutMultiplier() {
+    const numberOfMultiplier = document.querySelector(".amount-of-multipliers");
+    const multiplyCost = document.querySelector(".multiplier-cost");
+    const multiplierAmountText = document.querySelector(".multiplier-amount");
+    numberOfMultiplier.innerText = this.multiplierAmount;
+    multiplierAmountText.innerText = Math.ceil(this.multiplier);
+    this.multiplierCost = this.multiplierCost * 1.1;
+    multiplyCost.innerText = Math.round(this.multiplierCost);
   }
   updateDonutCount() {
     const donutCount = document.querySelector(".amount-of-donuts");
     donutCount.innerText = Math.round(this.count);
-    if((this.count)>=(this.autoClickersCost)){
+    if (this.count >= this.autoClickersCost) {
       this.showAutoClickerButton();
-    }if((this.count)<(this.autoClickersCost)){
+    }
+    if (this.count < this.autoClickersCost) {
       this.hideAutoClickerButton();
+    }
+    if (this.count >= this.multiplierCost) {
+      this.showMultiplierButton();
+    }
+    if (this.count < this.multiplierCost) {
+      this.hideMultiplierButton();
     }
   }
 }
